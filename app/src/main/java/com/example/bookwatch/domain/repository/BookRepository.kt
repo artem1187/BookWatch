4cf151f3aba6ffb44f14e43761f709bf76610e3b
artem1187 <bukinartem11@gmail.com>
2026-05-07 12:24:32 +0700

    Domain слой repository

---
+++ app/src/main/java/com/example/bookwatch/domain/repository/BookRepository.kt
@@ -0,0 +1,14 @@
+package com.example.bookwatch.domain.repository
+
+import com.example.bookwatch.data.remote.SearchBook
+import com.example.bookwatch.domain.model.Book
+import kotlinx.coroutines.flow.Flow
+
+interface BookRepository {
+    fun getAllBooks(): Flow<List<Book>>
+    suspend fun insertBook(book: Book)
+    suspend fun deleteSelectedBooks()
+    suspend fun updateBook(book: Book)
+    suspend fun searchBooks(query: String): List<SearchBook>
+    suspend fun getBookDetails(openLibraryKey: String): SearchBook?
+}
